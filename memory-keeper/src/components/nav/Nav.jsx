4e64da2da556f7165a8a146cd4e500e125d72708
TerryKrom <terrykrom1@gmail.com>
2025-09-27 23:42:08 -0300

    ADD: created modal

--- memory-keeper/src/components/nav/Nav.jsx
+++ memory-keeper/src/components/nav/Nav.jsx
@@ -1,19 +1,51 @@
-import React from 'react';
-import './Nav.css';
-import image from '../../assets/image.svg';
-import message from '../../assets/message.svg';
-import music from '../../assets/music.svg';
+import React, { useState } from "react";
+import "./Nav.css";
+import image from "../../assets/image.svg";
+import message from "../../assets/message.svg";
+import music from "../../assets/music.svg";
+import Modal from "../modal/Modal"; // importa o Modal.jsx
 
 const Nav = () => {
+    const [openModal, setOpenModal] = useState(null); // "image", "message", "music" ou null
+
+    const closeModal = () => setOpenModal(null);
+
     return (
         <div className="nav">
             <ul className="nav-icons">
-                <li><img src={image} alt="Image" /></li>
-                <li><img src={message} alt="Message" /></li>
-                <li><img src={music} alt="Music" /></li>
+                <li>
+                    <img src={image} alt="Image" onClick={() => setOpenModal("image")} />
+                </li>
+                <li>
+                    <img src={message} alt="Message" onClick={() => setOpenModal("message")} />
+                </li>
+                <li>
+                    <img src={music} alt="Music" onClick={() => setOpenModal("music")} />
+                </li>
             </ul>
+
+            {/* Modal customizado */}
+            <Modal isOpen={!!openModal} handleClose={closeModal}>
+                <div className="modal-panel">
+                    <h2 className="modal-title">
+                        {openModal === "image" && "Galeria de Imagens"}
+                        {openModal === "message" && "Mensagens"}
+                        {openModal === "music" && "Player de Música"}
+                    </h2>
+
+                    <div className="modal-body">
+                        {openModal === "image" && <p>Aqui você mostra imagens.</p>}
+                        {openModal === "message" && <p>Aqui ficam as mensagens.</p>}
+                        {openModal === "music" && <p>Aqui você toca músicas.</p>}
+                    </div>
+
+                    <button onClick={closeModal} className="btn-close">
+                        Fechar
+                    </button>
+                </div>
+            </Modal>
         </div>
     );
-}
+};
 
 export default Nav;
